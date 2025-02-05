72a6753f41d582ff415956997ad5dddfe03610a6
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-02-05 05:09:41 +0100

    added the new layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/ui/navbar";
 
 import "@/styles/global.css";
 
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable}`}>
-      <body className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <body className="min-h-screen transition-colors duration-300">
         <div className="flex flex-col min-h-screen">
           {/* Navbar Section */}
           <Navbar />
@@ -27,8 +27,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <main className="flex-1 container mx-auto p-6 mt-20">{children}</main>
 
           {/* Footer Section */}
-          <footer className="bg-gray-800 text-center p-4 rounded-t-2xl shadow-md">
-            <p className="text-sm font-mono text-gray-300">
+          <footer className="bg-card text-card-foreground text-center p-4 rounded-t-2xl shadow-md">
+            <p className="text-sm font-mono">
               © 2025 Ahmad FATAYERJI. All rights reserved.
             </p>
           </footer>
