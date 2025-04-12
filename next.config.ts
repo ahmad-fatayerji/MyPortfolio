e85bd4c2654dd4d96913c290dc2173eaeb22168e
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-12 23:22:46 +0200

    updated config for better prod

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  //output: 'export',
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/projects/index.txt',
+        destination: '/projects',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
