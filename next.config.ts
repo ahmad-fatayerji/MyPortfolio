73240a72a9756adeb14640e5733a9ae2cfca693e
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-13 01:49:35 +0200

    updated conf and yml

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: 'export',
   trailingSlash: true,
   async redirects() {
     return [
       {
-        source: '/projects/index.txt',
-        destination: '/projects',
+        source: "/projects/index.txt",
+        destination: "/projects",
         permanent: true,
       },
     ];
