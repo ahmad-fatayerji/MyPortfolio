476b92d845f22202e544481ae0f1dc52496cf927
FATAYERJI Ahmad <ahmad.fatayerji@etu.univ-nantes.fr>
2025-04-13 02:29:31 +0200

    updated config and removed turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  webpack: (config) => {
+    // Explicitly add the alias so that "@" maps to your "src" folder
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
   async redirects() {
     return [
       {
